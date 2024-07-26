6d1b9e95ab8fd9e033cbd3e4950579ee8bf2a928
Michael Jang <mjang01011@gmail.com>
2024-07-26 16:45:41 +0900

    [add] API Key validation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from utils import load_keys, create_travel_agent, increment_progress_bar, get_itinerary, get_updated_itinerary, get_directions, decode_route, create_map, update_map, connect_db, display_message
+from utils import load_keys, create_travel_agent, increment_progress_bar, decode_route, update_map, connect_db, display_message, verification_spinner
 from Router.Router import Router
 import streamlit as st
 from streamlit_folium import st_folium
@@ -9,10 +9,14 @@ from bson.binary import Binary
 import json
 
 
-keys = load_keys()
 st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
 title, login = st.columns([0.9, 0.1])
 title.title("Travel Planner")
+validation_result = verification_spinner()
+if not validation_result:
+    time.sleep(9999)
+keys = load_keys()
+    
 if 'auth' not in st.session_state:
     st.session_state.auth = False
 if not st.session_state.auth:
@@ -27,7 +31,7 @@ else:
 with st.form("travel_form"):
     input_query = st.text_input("Type your travel plan:")
     query_submitted = st.form_submit_button("Submit")
-select_llm_model = st.radio("Choose LLM Model:", ["gpt-3.5-turbo", "gemini-1.5-pro"])
+select_llm_model = st.radio("Choose language model:", ["gpt-3.5-turbo", "gemini-1.5-pro"])
 st.session_state.llm_model = select_llm_model
 llm_model_names = ["gpt-3.5-turbo", "gemini-1.5-pro"]
 llm_model_keys = ["open_ai_key", "google_gemini_key"]
@@ -43,7 +47,7 @@ if query_submitted:
             itinerary_col.subheader("Itinerary Details")
             progress_bar = map_col.progress(0,"Validating your travel plan...")
             travel_agent = create_travel_agent(keys[llm_model_keys[llm_model_names.index(st.session_state.llm_model)]], model=select_llm_model)
-            itinerary, list_of_places, validation = get_itinerary(travel_agent, input_query)
+            itinerary, list_of_places, validation = travel_agent.suggest_travel(input_query)
             st.session_state.input_query = input_query
             st.session_state.itinerary = itinerary
             st.session_state.list_of_places = list_of_places
@@ -52,7 +56,7 @@ if query_submitted:
             if validation:
                 increment_progress_bar(progress_bar, 0, 30, '\nRoute map is loading...')
                 router = Router(google_maps_key=keys["google_maps_key"])
-                directions_result, marker_points = get_directions(router, list_of_places)
+                directions_result, marker_points = router.make_markers(list_of_places)
                 increment_progress_bar(progress_bar, 30, 70, '\nRoute map is loading...')
                 route_coords = decode_route(directions_result)
                 if route_coords == None:
@@ -70,9 +74,7 @@ if query_submitted:
 
 def update_itinerary():
     travel_agent = create_travel_agent(keys[llm_model_keys[llm_model_names.index(st.session_state.llm_model)]], model=st.session_state.llm_model)
-    st.session_state.itinerary = get_updated_itinerary(travel_agent, st.session_state.input_query, json.dumps(st.session_state.list_of_places))
-    
-    
+    st.session_state.itinerary = travel_agent.update_itinerary(st.session_state.input_query, json.dumps(st.session_state.list_of_places))
     
 if 'initial_list_of_places' in st.session_state:        
     suggestions = []
--- utils.py
+++ utils.py
@@ -9,6 +9,9 @@ from pymongo import MongoClient
 from pymongo.server_api import ServerApi
 import streamlit as st
 from streamlit_modal import Modal
+import openai
+import requests
+import google.generativeai as genai
 
 def load_keys():
     load_dotenv(find_dotenv(), override=True)
@@ -19,14 +22,96 @@ def load_keys():
         "mongo_uri": os.environ.get("MONGO_URI")
     }
 
-def connect_db():
-    keys = load_keys()
-    client = MongoClient(keys["mongo_uri"], server_api=ServerApi('1'))
+
+def validate_openai_api_key(api_key):
+    client = openai.OpenAI(api_key=api_key)
+    try:
+        client.models.list()
+    except openai.AuthenticationError:
+        return False
+    else:
+        return True
+    
+def validate_gemini_api_key(api_key):
+    genai.configure(api_key=api_key)
+    model = genai.GenerativeModel("gemini-1.5-pro")
+    try:
+        model.generate_content("Hello World")
+    except:
+        return False
+    else:
+        return True
+    
+def validate_google_maps_key(api_key):
+    url = f"https://maps.googleapis.com/maps/api/geocode/json?address=New+York&key={api_key}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        if data.get('status') == 'OK':
+            return True
+        else:
+            return False
+    else:
+        return False
+
+def validate_mongo_uri(mongo_uri):
+    client = MongoClient(mongo_uri, server_api=ServerApi('1'))
     try:
         client.admin.command('ping')
-        print("Pinged your deployment. You successfully connected to MongoDB!")
     except Exception as e:
-        print(e)
+        return False
+    return client
+
+def verification_spinner():
+    with st.spinner('Verifying keys...'):
+        keys = load_keys()
+        openai_message = st.empty()
+        gemini_message = st.empty()
+        google_maps_message = st.empty()
+        mongo_message = st.empty()
+        validation_results = {"openai": True, "gemini": True, "google_maps": True, "mongo_uri": True}
+
+        if validate_openai_api_key(keys["open_ai_key"]):
+            openai_message.success("OpenAI API Key is valid.")
+        else:
+            openai_message.warning("OpenAI API Key is not valid.")
+            validation_results["openai"] = False
+
+        if validate_gemini_api_key(keys["google_gemini_key"]):
+            gemini_message.success("Gemini API Key is valid.")
+        else:
+            gemini_message.warning("Gemini API Key is not valid.")
+            validation_results["gemini"] = False
+
+        if validate_google_maps_key(keys["google_maps_key"]):
+            google_maps_message.success("Google Maps API Key is valid.")
+        else:
+            google_maps_message.warning("Google Maps API Key is not valid.")
+            validation_results["google_maps"] = False
+
+        if validate_mongo_uri(keys["mongo_uri"]):
+            mongo_message.success("Mongo URI is valid.")
+        else:
+            mongo_message.warning("Mongo URI is not valid.")
+            validation_results["mongo_uri"] = False
+        time.sleep(1)
+
+    openai_message.empty()
+    gemini_message.empty()
+    google_maps_message.empty()
+    mongo_message.empty()
+    if (validation_results["openai"] or validation_results["gemini"]) and validation_results["google_maps"] and validation_results["mongo_uri"]:
+        return True
+    if not validation_results["openai"] and not validation_results["gemini"]:
+        st.warning("You must provide at least one valid API key for either OpenAI or Gemini.")
+    if not validation_results["google_maps"]:
+        st.warning("You must provide a valid API key for Google Maps.")
+    if not validation_results["mongo_uri"]:
+        st.warning("You must provide a valid key for mongo_uri.")
+    return False
+
+def connect_db():
+    client = validate_mongo_uri(load_keys()["mongo_uri"])
     db = client["TravelPlanner"]
     users_collection = db["users"]
     return users_collection
@@ -44,21 +129,10 @@ def increment_progress_bar(progress_bar, prev, final, text):
     for i in range(prev, final):
         progress_bar.progress(i, text)
         time.sleep(0.01)
-        
-def get_itinerary(travel_agent, query):
-    return travel_agent.suggest_travel(query)
-
-def get_updated_itinerary(travel_agent, query, mapping_list):
-    return travel_agent.update_itinerary(query, mapping_list)
-
-def get_directions(router, list_of_places):
-    directions_result, marker_points = router.make_markers(list_of_places)
-    return directions_result, marker_points
 
 def decode_route(directions_result):
     if not directions_result or not directions_result[0].get("overview_polyline"):
         return None
-    
     overall_route = decode_polyline(directions_result[0]["overview_polyline"]["points"])
     return [(float(p["lat"]), float(p["lng"])) for p in overall_route]
 
@@ -87,7 +161,7 @@ def create_map(route_coords, marker_points, map_start_loc):
 
 def update_map(list_of_places, google_maps_key):
     router = Router(google_maps_key=google_maps_key)
-    directions_result, marker_points = get_directions(router, list_of_places)
+    directions_result, marker_points = router.make_markers(list_of_places)
     route_coords = decode_route(directions_result)
     if route_coords == None:
         return st.warning("Error has occured with google maps API. Please try again.")
